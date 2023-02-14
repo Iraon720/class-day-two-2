46182f83850d9a8bc5d3b825296511b74433ebf6
Iraon720 <iraon3493@gmail.com>
2023-02-14 01:27:07 -0500

    Changed from strict equality to eqaulity

--- src/App.js
+++ src/App.js
@@ -1,59 +1,68 @@
 import React from 'react';
 import './App.css';
-import TodoContainer from './components/TodoContainer'
-import TodoInput from './components/TodoInput'
+import TodoContainer from './components/TodoContainer';
+import TodoInput from './components/TodoInput';
 
-class App extends React.Component{
-
+class App extends React.Component {
   //todo app structure {status, description, title, id}
-  constructor(){
+  constructor() {
     super();
-    this.state={
-      todos:[
+    this.state = {
+      todos: [
         {
           id: 1,
-          title: "test title",
-          description: "another test descripion",
-          status: 'PENDING'
-      }
-    ],
-      currentlyEditingTodo: null
-    }
-    this.addTodo = this.addTodo.bind(this)
+          title: 'test title',
+          description: 'another test descripion',
+          status: 'PENDING',
+        },
+      ],
+      currentlyEditingTodo: null,
+    };
+    this.addTodo = this.addTodo.bind(this);
+    this.deleteTodo = this.deleteTodo.bind(this);
+    this.editTodo = this.editTodo.bind(this);
   }
 
-  addTodo(todoObject){
+  addTodo(todoObject) {
     debugger;
-    const newTodos = JSON.parse(JSON.stringify(this.state.todos))
-    newTodos.push(todoObject)
+    const newTodos = JSON.parse(JSON.stringify(this.state.todos));
+    newTodos.push(todoObject);
     return this.setState({
-      todos: newTodos
-    })
+      todos: newTodos,
+    });
   }
 
-  editTodo(){
-    // finish me
+  editTodo(updatedTodo) {
+    const newTodos = this.state.todos.map((todo) => {
+      if (todo.id === updatedTodo.id) {
+        return { ...todo, ...updatedTodo };
+      }
+      return todo;
+    });
+    this.setState({ todos: newTodos });
   }
 
-  deleteTodo(){
-    // finish me
+  deleteTodo = (todoDelete) {
+    this.setState({
+      todos: this.state.todos.filter((todo) => todo.id == todoDelete.id),
+    });
   }
 
-  handleEditCallBack = todo => this.setState({currentlyEditingTodo: todo})
+  handleEditCallBack = (todo) => this.setState({ currentlyEditingTodo: todo });
 
-  render(){
-    return(
+  render() {
+    return (
       <div>
         <TodoInput
           todo={this.state.currentlyEditingTodo}
           todoAmount={this.state.todos.length}
           handleSaveClick={this.addTodo}
-          buttonText={this.state.currentlyEditingTodo? 'Edit':'Add'}
+          buttonText={this.state.currentlyEditingTodo ? 'Edit' : 'Add'}
         />
         <TodoContainer
-          todos = {this.state.todos}
-          handleEdit = {this.editTodo}
-          handleDelete = {this.deleteTodo}
+          todos={this.state.todos}
+          handleEdit={this.editTodo}
+          handleDelete={this.deleteTodo}
         />
       </div>
     );
